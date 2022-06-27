b02494e62adde372bd800c004927c0edfad0104f
BorodinAG <borodin_ag@bk.ru>
2022-06-28 00:12:39 +0300

    new

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.dao.CarsDao;
 
 @Controller
 @RequestMapping("/cars")
 public class CarsController {
-
     private final CarsDao carsDao;
 
     @Autowired
@@ -19,8 +20,15 @@ public class CarsController {
     }
 
     @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("auto", carsDao.index());
-        return "cars/index";
+    public String index(@RequestParam(value = "count", defaultValue = "0") int count, Model model) {
+        if (count >= 1 && count < 5) {
+            model.addAttribute("auto", carsDao.indexCar(count));
+            return "cars/index";
+        } else {
+            model.addAttribute("auto", carsDao.index());
+            return "cars/index";
+        }
     }
+
 }
+
--- src/main/java/web/dao/CarsDao.java
+++ src/main/java/web/dao/CarsDao.java
@@ -12,14 +12,29 @@ public class CarsDao {
 
     private static int COUNT;
     private List<Cars> auto;
+    private List<Cars> autoCar;
 
     {
         auto = new ArrayList<>();
 
-        auto.add(new Cars(++COUNT,"Chevrolet","Lacetti",2009));
-        auto.add(new Cars(++COUNT,"Toyota","Camry",2020));
-        auto.add(new Cars(++COUNT,"Hyundai","Creta",2017));
+        auto.add(new Cars(++COUNT, "Chevrolet", "Lacetti", 2009));
+        auto.add(new Cars(++COUNT, "Toyota", "Camry", 2020));
+        auto.add(new Cars(++COUNT, "Hyundai", "Creta", 2017));
+        auto.add(new Cars(++COUNT, "BMW", "X7", 2021));
+        auto.add(new Cars(++COUNT, "Mercedes", "Benz CLA", 2022));
+        auto.add(new Cars(++COUNT, "CHERY", "TIGGO 8", 2020));
+        auto.add(new Cars(++COUNT, "LADA", "Vesta", 2020));
     }
 
-    public List<Cars> index(){ return auto;}
+    public List<Cars> index() {
+        return auto;
+    }
+
+    public List<Cars> indexCar(int count) {
+        return auto.subList(0, count);
+    }
+
+    public Cars show(int id) {
+        return auto.stream().filter(cars -> cars.getId() == id).findAny().orElse(null);
+    }
 }
